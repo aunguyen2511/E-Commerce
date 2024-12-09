63fcc84b430002a9ee213a7336bad2b38b8f1902
aunguyen2511 <aunguyen2511@gmail.com>
2024-12-09 23:58:41 +0700

    new view, not done logic yet

---
+++ MyEStore/MyEStore/Entities/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyEStore.Entities;
+
+public partial class Role
+{
+    public string Rid { get; set; } = null!;
+
+    public string NameRole { get; set; } = null!;
+
+    public string NormalizedName { get; set; } = null!;
+
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
+}
---
+++ MyEStore/MyEStore/Models/Services/IVnPayService.cs
@@ -0,0 +1,8 @@
+﻿namespace MyEStore.Models.Services
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(HttpContext context, VnPaymentRequestModel model);
+        VnPaymentResponseModel PaymentExecute(IQueryCollection collections);
+    }
+}

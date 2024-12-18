0d81e03d65cde0f5f89307b74fe40c9225f4fc66
huynhlethanhnhan <thanhnhanhuynh1807@gmail.com>
2024-12-18 19:59:36 +0700

    Update Cart and Payment(Layout not fix style)

---
+++ UpdateCode/ECommerceMVC/Data/CartItemDb.cs
@@ -0,0 +1,14 @@
+﻿namespace ECommerceMVC.Data
+{
+    public class CartItemDb
+    {
+        public int Id { get; set; }
+        public string MaKh { get; set; } // Mã khách hàng
+        public int MaHh { get; set; } // Mã hàng hóa
+        public string TenHH { get; set; }
+        public double DonGia { get; set; }
+        public string Hinh { get; set; }
+        public int SoLuong { get; set; }
+        public DateTime NgàyTạo { get; set; } = DateTime.Now;
+    }
+}
---
+++ UpdateCode/ECommerceMVC/ViewModels/CartModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ECommerceMVC.ViewModels
+{
+    public class CartModel
+    {
+        public int Quantity { get; set; }
+        public double Total { get; set; }
+        public List<CartItem> CartItems { get; set; } // Danh sách sản phẩm trong giỏ hàng
+    }
+}
